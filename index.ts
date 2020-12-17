dce049d38ab454a46486200cd367ef93a7a43d27
Anwesh43 <anweshthecool0@gmail.com>
2020-12-17 17:54:37 +0530

    adding fixes

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import { countBy } from "lodash"
-
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const parts : number = 4 
@@ -76,6 +74,7 @@ class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D 
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w 
@@ -87,11 +86,14 @@ class Stage {
     render() {
         this.context.fillStyle = backColor 
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
